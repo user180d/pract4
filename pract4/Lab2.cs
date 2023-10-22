ad34766e710af505bb1b05d5772eabeb78840aee
D T <golub1mail@gmail.com>
2023-10-22 16:05:20 +0300

    Add project files.

---
+++ pract4/Lab2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract4
+{
+    internal class Lab2 : MasterLab
+    {
+        string inputFile;
+        string outputFile;
+        int answer = 0;
+        public Lab2(string i, string o)
+        {
+            inputFile = i;
+            outputFile = o;
+        }
+        void entry(int shop, int steps)
+        {
+            if (steps == shop)
+            {
+                answer = 1;
+                return;
+            }
+            else if (shop == steps - 1)
+            {
+                answer = 0;
+                return;
+            }
+            else if ((steps - shop) % 2 != 0)
+            {
+                answer = 0;
+                return;
+            }
+            else if (steps == shop + 4)
+            {
+                float n = shop, a = 0;
+                a += (n + 3) * (n / 2);
+                answer = (int)a;
+                return;
+            }
+            else
+            {
+                solve(shop, steps);
+            }
+        }
+
+        void solve(int shop, int steps)
+        {
+
+            if (shop == 0)
+            {
+                return;
+            }
+            else if (shop == steps - 2)
+            {
+                answer += shop;
+            }
+            else
+            {
+                solve(shop - 1, steps - 1);
+                solve(shop + 1, steps - 1);
+            }
+        }
+        public override bool start()
+        {
+            string[] input = File.ReadAllText(inputFile).Split();
+            try
+            {
+
+                int n = int.Parse(input[0]);
+
+                int k = int.Parse(input[1]);
+                if (n != 0 && n <= k && k <= 37)
+                {
+                    entry(n, k);
+                    Console.WriteLine(answer);
+                    File.WriteAllText(outputFile, answer.ToString());
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Distance to shop equal 0, bigger than amount of steps or some of paramets bigger than 37");
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+    }
+}
---
+++ pract4/Program.cs
@@ -0,0 +1,181 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Xml;
+using McMaster.Extensions.CommandLineUtils;
+using pract4;
+
+class Program
+{
+    MasterLab lab;
+    string LAB_PATH;
+    string inputFile, outputFile;
+    static void Main(string[] args)
+        => CommandLineApplication.Execute<Program>(args);
+    void printlab() 
+    {
+        Console.WriteLine("Enter -i or --input + path, -o or --output + path if needed\n Menu: \nlab1 - process lab 1 \nlab2 - process lab 2 \nlab3 - process lab 3 \nexit - close app");
+    }
+    void saveIandO(string[] t)
+    {
+        int i = 0;  
+        if (t[1] == "-i" || t[1] == "--input")
+        {
+            inputFile = t[2];
+            i++;
+        }
+        if (t[3] == "-o" || t[3] == "--output")
+        {
+            outputFile = t[4];
+            i++;
+        }
+
+    }
+    string  findType(int l)
+    {
+        for (int i = 0; i < l; i++)
+        {
+            if (LAB_PATH[i] == '/' )
+            {
+                return "/";
+            }
+            if (LAB_PATH[i].ToString() == @"\")
+            {
+                return @"\";
+            }
+        }
+        return "";
+    }
+    void checkFormat() 
+    {
+        int l = LAB_PATH.Length;
+        if (LAB_PATH[l-1] != '/' || LAB_PATH[l-1].ToString() != @"\")
+        {
+            LAB_PATH += findType(l);
+        }
+    }
+    void useDirPath()
+    {
+        checkFormat();
+        inputFile= LAB_PATH+"INPUT.txt";
+        outputFile = LAB_PATH +"OUTPUT.txt";
+    }
+    bool checkHomeDir()
+    {
+        LAB_PATH = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        useDirPath();
+        if (File.Exists(inputFile))
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    void process(string str)
+    {
+        bool operSuccess = true;
+        if (inputFile == null) 
+        {
+            if (LAB_PATH != null)
+            {
+                useDirPath();
+            }
+            else 
+            {
+                if (checkHomeDir()==false) {
+                    str = "empty";
+                }
+            }
+        }
+        switch (str)
+        {
+            case "empty": Console.WriteLine("No file to work with"); break;
+            case "exit":
+                break;
+            case "lab1":
+                Console.WriteLine("=============== LAB_1 ===============");
+                lab = new Lab1(inputFile,outputFile);
+                operSuccess = lab.start();
+                if (operSuccess != false)
+                {
+                    Console.Write("Result - Success");
+                    break;
+                }
+                Console.Write("Result - Program crusshed returning to the main menu");
+                break;
+
+            case "lab2":
+                Console.WriteLine("=============== LAB_2 ===============");
+                lab = new Lab2(inputFile, outputFile);
+                operSuccess = lab.start();
+                if (operSuccess != false)
+                {
+                    Console.Write("Result - Success");
+                    break;
+                }
+                Console.Write("Result - Program crusshed returning to the main menu");
+                break;
+
+            case "lab3":
+                Console.WriteLine("=============== LAB_3 ===============");
+                lab = new Lab3(inputFile, outputFile);
+                operSuccess = lab.start();
+                if (operSuccess != false)
+                {
+                    Console.Write("Result - Success");
+                    break;
+                }
+                Console.Write("Result - Program crusshed returning to the main menu");
+                break;
+                
+        }
+    }
+    void saveDir(string[] t)
+    {
+        if (t[1] == "-p" || t[1]=="--path")
+        {
+            LAB_PATH = t[2];
+        }
+    }
+    void OnExecute()
+    {
+        string[] token=null;
+        bool correcctInput=false;
+        string menu = "";
+        while (menu != "exit")
+        {
+            
+            Console.WriteLine("\nPlease enter: \n    'run' to start\n    'set-path -p' - path to a with input and output files, -p is a path \n    'exit' to close app\n    'version' - additional info ");
+            while (!correcctInput) {
+                menu = Console.ReadLine();
+                token = menu.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (token[0] == "exit" || token[0] == "version" || token[0] == "run" || (token[0] == "set-path" && token.Length==3))
+                { 
+                    correcctInput= true;
+                }
+            }
+            correcctInput  = false;
+            switch (token[0])
+            {
+                
+                case "exit": break;
+                case "version": Console.WriteLine("Author - D Taranov\nVersion: 1.0.0\n"); break;
+                case "set-path":
+                    if (token.Length == 3)
+                    {
+                        saveDir(token);
+                    }
+                    break;
+                case "run":
+                    printlab();
+                    menu = Console.ReadLine();
+                    token = menu.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (token.Length == 5)
+                    {
+                        saveIandO(token);
+                    }
+                    process(token[0]);
+                    break;
+            }
+        }
+    }
+}
